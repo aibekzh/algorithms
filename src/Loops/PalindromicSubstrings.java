04044f12da4343df34338298e6d1049b47563bdc
Айбек Жумаш <aibek_zhumash@mail.ru>
2021-07-25 23:59:41 +0600

    palindromic subtrings solved

--- src/Loops/PalindromicSubstrings.java
+++ src/Loops/PalindromicSubstrings.java
@@ -1,12 +1,37 @@
 package Loops;
 
+import java.util.Arrays;
+
 public class PalindromicSubstrings {
 
     public String link = "https://leetcode.com/problems/palindromic-substrings/";
 
-    public int countSubstrings(String s) {
+    public static int countSubstrings(String s) {
+        int count = 0;
         char[] arr = s.toCharArray();
+        int[][] matrix = new int[arr.length][arr.length];
 
-        return 0;
+        for (int[] row : matrix) {
+            Arrays.fill(row, 0);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            matrix[i][i] = 1;
+            count++;
+        }
+
+        for (int col = 1; col < arr.length; col++) {
+            for (int row = 0; row < col; row++) {
+                if (col - row == 1 && arr[row] == arr[col]) {
+                    count++;
+                    matrix[row][col] = 1;
+                } else if (matrix[row + 1][col - 1] == 1 && arr[row] == arr[col]) {
+                    count++;
+                    matrix[row][col] = 1;
+                }
+            }
+        }
+
+        return count;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,13 @@
 package com.company;
 
+import Loops.PalindromicSubstrings;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "asdasd";
-        char[] arr = s.toCharArray();
-        int[][] matrix = new int[arr.length][arr.length];
-
-        for (int[] row : matrix){
-            Arrays.fill(row, 0);
-        }
-
-        System.out.println(Arrays.deepToString(matrix));
-
+        int count = PalindromicSubstrings.countSubstrings("aaabbaaaababbaabbbaaaabd");
+        System.out.println(count);
     }
 }
